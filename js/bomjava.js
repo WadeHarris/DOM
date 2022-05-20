32f140ca9b1b442a6d84054e0439e75a319e0cd6
WadeHarris <97800110+WadeHarris@users.noreply.github.com>
2022-05-20 23:57:51 +0200

    Add files via upload

---
+++ js/bomjava.js
@@ -0,0 +1,30 @@
+//java script for the DOM activity
+//Use the same logoc as creating a list
+
+const input = document.querySelector("#favchap");
+
+const button = document.querySelector("button");
+
+const list = document.querySelector(".list");
+
+
+button.addEventListener("click", function() {
+    
+    userInput = input.value;
+    input.value = "";
+
+    const listItem = document.createElement("li");
+    const itemButton = document.createElement("button");
+
+    listItem.textContent = userInput;
+    itemButton.textContent = "❌"; 
+    
+    listItem.appendChild(itemButton);
+    list.appendChild(listItem);
+
+    itemButton.addEventListener("click", function() {
+        list.removeChild(listItem);
+    });
+
+    input.focus();
+});
